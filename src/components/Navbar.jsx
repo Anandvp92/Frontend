2e6e540f8d2320d360bc8ee0584a804a3b07b677
Anandvp <anandvp92@gmail.com>
2024-02-16 16:39:07 +0530

    inital commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,39 @@
 import Favicon from "react-favicon";
-import titleicon from '../assets/home.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
+import titleicon from '../assets/home.svg';
+import { useState } from "react";
 export function Navbar(){
+   const[currentState,changeState]=useState(true);
+   const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 700);
+
+   const clickfunc =()=>{
+    changeState(!currentState)
+    setIsSmallScreen(!isSmallScreen)
+    console.log(isSmallScreen)
+   }
+
+   window.addEventListener('resize',handleResize);
     return(
         <>
        <Favicon url={titleicon} />
         <div className="navbar">
+        <div className="logoimage" >
+        <a href="/home"><img src="https://static.vecteezy.com/system/resources/thumbnails/024/684/356/small/illustration-of-eagle-head-logo-with-ai-generative-free-png.png" className="logo"/></a>
 
-<a href="/home"><img src="https://static.vecteezy.com/system/resources/thumbnails/024/684/356/small/illustration-of-eagle-head-logo-with-ai-generative-free-png.png" className="logo"/></a>
-       
-
-        <div className="navlinks"><a className="links" href="/home">Home</a></div>
+        </div>
+        <div className="hambergermenu" >
+        <FontAwesomeIcon onClick={clickfunc}  icon={ currentState? faBars : faTimes}></FontAwesomeIcon>
+        </div>
+      
+        <div className="navlinks "><a className="links" href="/home">Home</a></div>
         <div className="navlinks"><a className="links" href="/product">product</a></div>
         <div className="navlinks"><a className="links" href="/blog">blog</a></div>
         <div className="navlinks"><a className="links" href="/about">about</a></div>
         <div className="navlinks"><a className="links" href="/contact">Contact</a></div>
+    
+
+       
         </div>
         </>
     )
